a75641500bbf9b95f2638cd47b0c66ca2de7b2f8
Tomasz <snakerrek@gmail.com>
2022-03-12 13:18:06 +0100

    Updated skills section to not show scrollDown

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const Skills = ({ skillsData }: Props): JSX.Element => {
   return (
-    <SectionWrapper title={"My skills"} scrollDown={true}>
+    <SectionWrapper title={"My skills"} scrollDown={false}>
       <SkillsHolder>
         {skillsData.map((skill, index) => {
           return (
