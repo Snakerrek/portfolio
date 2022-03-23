baacffe24b625c455a904d922742f0b6c91b4e02
Tomasz <snakerrek@gmail.com>
2022-03-23 21:30:28 +0100

    Added arrow to footer that allows to go back to top of the website

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,10 +1,37 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const arrowAnimation = keyframes`
+  0% {
+    transform: scale3d(1,1,1);
+  }
+
+  50% {
+    transform: scale3d(0.8,0.8,0.8);
+  }
+
+  100% {
+    transform: scale3d(1,1,1);
+  }
+`;
 
 export const FooterWrapper = styled.footer`
+  position: relative;
   background: var(--black);
   text-align: center;
+
   p {
     font-size: var(--fontSmall);
     margin: 10px;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  div {
+    position: absolute;
+    right: 20px;
+    bottom: 10px;
+    font-size: 3rem;
+    animation: ${arrowAnimation} 2s ease infinite;
   }
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { FooterWrapper } from "./Footer.styles";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const Footer = (): JSX.Element => {
   const currentYear = new Date().getFullYear();
@@ -8,6 +9,11 @@ const Footer = (): JSX.Element => {
     <FooterWrapper>
       <p>Created by Tomasz Kraszewski</p>
       <p>Copyright {currentYear} - All rights reserved</p>
+      <div>
+        <a href="/#">
+          <BsFillArrowUpCircleFill />
+        </a>
+      </div>
     </FooterWrapper>
   );
 };
