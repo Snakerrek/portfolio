5f2b45f3e7c832aa627d7c7ff84a07e1bcbf3776
Tomasz <snakerrek@gmail.com>
2022-03-29 19:48:43 +0200

    Now on mobile project links open in same card

--- src/components/ProjectModal/ProjectLinks.tsx
+++ src/components/ProjectModal/ProjectLinks.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Links, Link } from "./ProjectLinks.styles";
+import { isOnMobile } from "../../helpers";
 
 type Props = {
   githubLink: string;
@@ -8,18 +9,33 @@ type Props = {
 };
 
 const ProjectButtons = ({ githubLink, liveLink }: Props): JSX.Element => {
-  return (
-    <Links>
-      <a target="_blank" rel="noopener noreferrer" href={githubLink}>
-        <Link linkType="Code">Code</Link>
-      </a>
-      {liveLink !== "" && (
-        <a target="_blank" rel="noopener noreferrer" href={liveLink}>
-          <Link linkType="Live">Live</Link>
+  if (isOnMobile()) {
+    return (
+      <Links>
+        <a href={githubLink}>
+          <Link linkType="Code">Code</Link>
         </a>
-      )}
-    </Links>
-  );
+        {liveLink !== "" && (
+          <a href={liveLink}>
+            <Link linkType="Live">Live</Link>
+          </a>
+        )}
+      </Links>
+    );
+  } else {
+    return (
+      <Links>
+        <a target="_blank" rel="noopener noreferrer" href={githubLink}>
+          <Link linkType="Code">Code</Link>
+        </a>
+        {liveLink !== "" && (
+          <a target="_blank" rel="noopener noreferrer" href={liveLink}>
+            <Link linkType="Live">Live</Link>
+          </a>
+        )}
+      </Links>
+    );
+  }
 };
 
 export default ProjectButtons;
